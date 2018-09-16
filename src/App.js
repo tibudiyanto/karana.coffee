06d44296495fa7285e46129d9a03a06749a11456
theo <theodorusib@gmail.com>
2018-09-16 11:24:41 +0700

    Added service worker

--- src/App.js
+++ src/App.js
@@ -132,6 +132,15 @@ const Order = ({ formUrl }) => {
 };
 
 class App extends Component {
+  componentDidMount() {
+    // Check that service workers are registered
+    if ("serviceWorker" in navigator) {
+      // Use the window load event to keep the page load performant
+      window.addEventListener("load", () => {
+        navigator.serviceWorker.register("/sw.js");
+      });
+    }
+  }
   render() {
     const { formUrl } = this.props;
     return (
--- static.config.js
+++ static.config.js
@@ -1,3 +1,5 @@
+const workbox = require("workbox-webpack-plugin");
+
 // Though this file is optional, there are SO MANY COOL THINGS you can do here.
 // Read the docs at https://github.com/nozzle/react-static/blob/master/README.md to learn more!
 export default {
@@ -7,5 +9,9 @@ export default {
       tahik: "kontol",
       formUrl: process.env.FORM_URL || "google.com"
     };
+  },
+  webpack: config => {
+    config.plugins.push(new workbox.GenerateSW());
+    return config;
   }
 };
